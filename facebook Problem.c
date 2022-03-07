10fee11543bc83725dfe960cbfe3ba5dfd50701c
hishamized <mirhashim2001@gmail.com>
2022-03-07 13:33:31 +0530

    facebook

--- facebook Problem.c
+++ facebook Problem.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main(){
     int id;
-    char name[20];
+    char name[30];
     printf("==========INFORMATION==========");
     printf("\n");
     printf("   Input ID[4]   =   ", id);
     scanf("%d", &id);
     printf("      Input Name   =   ", name);
     getchar();
-    fgets(name, 20, stdin);
+    fgets(name, 30, stdin);
     printf("================================\n");
     printf("\n");
     printf("------ID------NAME------ \n");
